cd30f6e780f2eea44860809c537c2efa6926ea38
shreyansh90 <88185055+shreyansh90@users.noreply.github.com>
2024-04-15 17:41:48 +0530

    Update musician_controller.go
    update

--- controllers/musician_controller.go
+++ controllers/musician_controller.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/shreyansh90/jukebox-app/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -18,8 +19,7 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	ctx := context.TODO()
 	err = client.Connect(ctx)
 	if err != nil {
 		panic(err)
@@ -27,7 +27,7 @@ func init() {
 	musicianCollection = client.Database("jukebox").Collection("musicians")
 }
 
-func CreateMusician(c *gin.Context) {
+func CreateOrUpdateMusician(c *gin.Context) {
 	var musician models.Musician
 	if err := c.ShouldBindJSON(&musician); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -38,5 +38,41 @@ func CreateMusician(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
 		return
 	}
-	c.JSON(http.StatusCreated, gin.H{"message": "Musician created successfully", "data": musician})
+	c.JSON(http.StatusCreated, gin.H{"message": "Musician created/updated successfully", "data": musician})
+}
+
+func GetMusiciansForAlbumSortedByName(c *gin.Context) {
+	// Get album ID from path parameters
+	albumID := c.Param("album_id")
+
+	var musicians []models.Musician
+
+	options := options.Find().SetSort(bson.D{{"name", 1}})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	filter := bson.D{{"albums", albumID}}
+	cursor, err := musicianCollection.Find(ctx, filter, options)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var musician models.Musician
+		if err := cursor.Decode(&musician); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+			return
+		}
+		musicians = append(musicians, musician)
+	}
+
+	if err := cursor.Err(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": musicians})
 }
