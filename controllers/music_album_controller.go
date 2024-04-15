62f487d1f18347f106b66448a206b5e40f0a9849
shreyansh90 <88185055+shreyansh90@users.noreply.github.com>
2024-04-15 17:41:03 +0530

    Update music_album_controller.go
    Update

--- controllers/music_album_controller.go
+++ controllers/music_album_controller.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/shreyansh90/jukebox-app/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -18,8 +19,7 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	ctx := context.TODO()
 	err = client.Connect(ctx)
 	if err != nil {
 		panic(err)
@@ -27,7 +27,7 @@ func init() {
 	musicAlbumCollection = client.Database("jukebox").Collection("musicAlbums")
 }
 
-func CreateMusicAlbum(c *gin.Context) {
+func CreateOrUpdateAlbum(c *gin.Context) {
 	var musicAlbum models.MusicAlbum
 	if err := c.ShouldBindJSON(&musicAlbum); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -38,5 +38,74 @@ func CreateMusicAlbum(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
 		return
 	}
-	c.JSON(http.StatusCreated, gin.H{"message": "Music album created successfully", "data": musicAlbum})
+	c.JSON(http.StatusCreated, gin.H{"message": "Music album created/updated successfully", "data": musicAlbum})
+}
+
+func GetAlbumsSortedByReleaseDate(c *gin.Context) {
+	var albums []models.MusicAlbum
+
+	options := options.Find().SetSort(bson.D{{"release_date", 1}})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cursor, err := musicAlbumCollection.Find(ctx, bson.D{}, options)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var album models.MusicAlbum
+		if err := cursor.Decode(&album); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+			return
+		}
+		albums = append(albums, album)
+	}
+
+	if err := cursor.Err(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	// Return the sorted list of albums
+	c.JSON(http.StatusOK, gin.H{"data": albums})
+}
+
+func GetAlbumsForMusicianSortedByPrice(c *gin.Context) {
+	// Get musician ID from path parameters
+	musicianID := c.Param("musician_id")
+
+	var albums []models.MusicAlbum
+
+	options := options.Find().SetSort(bson.D{{"price", 1}})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	filter := bson.D{{"musicians", musicianID}}
+	cursor, err := musicAlbumCollection.Find(ctx, filter, options)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var album models.MusicAlbum
+		if err := cursor.Decode(&album); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+			return
+		}
+		albums = append(albums, album)
+	}
+
+	if err := cursor.Err(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": albums})
 }
